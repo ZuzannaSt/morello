1febf9c0a0c284093575c37d3e888e542d1deb47
zustmust <zuzanna.st@gmail.com>
2016-01-04 21:31:02 +0100

    Add remote host

--- web/config.php
+++ web/config.php
@@ -7,7 +7,8 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-    '149.156.175.52'
+    '149.156.175.52',
+    '87.207.119.138'
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
