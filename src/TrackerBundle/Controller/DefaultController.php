6165105cd10a11946a1c58a15bbd5ffd38d6f96e
Zuzanna Stolińska <zuzanna.st@gmail.com>
2016-01-04 21:17:48 +0100

    Add Sensio routing and use Twig view

--- src/TrackerBundle/Controller/DefaultController.php
+++ src/TrackerBundle/Controller/DefaultController.php
@@ -3,9 +3,15 @@
 namespace TrackerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/tracker/{name}")
+     */
     public function indexAction($name)
     {
         return $this->render('TrackerBundle:Default:index.html.twig', array('name' => $name));
