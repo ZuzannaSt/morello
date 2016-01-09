d8907443dcfc4e1053b440a021a0952ba2c67dc3
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-01-09 17:55:53 +0100

    Create User entity and interface

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\UserRepository")
+ */
+ class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(
+     *     type="integer",
+     *     nullable=false,
+     *     options={
+     *      "unsigned"=true
+     *     }
+     * )
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(
+     *     name="password",
+     *     type="string",
+     *     length=128,
+     *     nullable=false,
+     * )
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(
+     *     name="username",
+     *     type="string",
+     *     length=128,
+     *     unique=true,
+     *     nullable=false
+     * )
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(
+     *     name="email",
+     *     type="string",
+     *     length=128,
+     *     unique=true,
+     *     nullable=false
+     * )
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(
+     *     name="is_active",
+     *     type="boolean"
+     *     )
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(
+     *     name="firstname",
+     *     type="string",
+     *     length=128,
+     *     nullable=true
+     * )
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(
+     *     name="lastname",
+     *     type="string",
+     *     length=128,
+     *     nullable=false
+     * )
+     */
+    private $lastName;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+    }
+    
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+        ));
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+        ) = unserialize($serialized);
+    }
+}
