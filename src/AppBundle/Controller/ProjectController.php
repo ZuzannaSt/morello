ab0ca553b58afa75ed82c7efabed6d792c47765d
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-01-18 19:55:56 +0100

    I wanna make it better

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -1,223 +1,147 @@
 <?php
 
 namespace AppBundle\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use AppBundle\Form\ProjectType;
-use AppBundle\Form\ProjectEdit;
-use AppBundle\Entity\Project;
 
-class ProjectController extends Controller
+use AppBundle\Form\ProjectType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @Route(service="app.projects_controller")
+ */
+class ProjectController
 {
+    private $translator;
+    private $templating;
+    private $session;
+    private $router;
+    private $model;
+    private $formFactory;
+
+    public function __construct(
+        Translator $translator,
+        EngineInterface $templating,
+        Session $session,
+        RouterInterface $router,
+        ObjectRepository $model,
+        FormFactory $formFactory
+    ) {
+        $this->translator = $translator;
+        $this->templating = $templating;
+        $this->session = $session;
+        $this->router = $router;
+        $this->model = $model;
+        $this->formFactory = $formFactory;
+    }
+
     /**
      * @Route("/projects", name="projects")
      */
     public function indexAction()
     {
-        $projects = $this->get('app.project_repository')
-            ->findAllOrderedByName();
+        $projects = $this->model->findAllOrderedByName();
 
-        return $this->render(
+        return $this->templating->renderResponse(
             'AppBundle:Project:index.html.twig',
             array('projects' => $projects)
         );
     }
 
      /**
-     * @Route("projects/show/{id}", name="project_show")
-     * @Method("GET")
-     * @Template()
+     * @Route("projects/view/{id}", name="project_view")
      */
 
      public function viewAction($id)
      {
-       $em = $this->getDoctrine()->getManager();
-
-       $project = $em->getRepository('AppBundle:Project')->find($id);
-
-       if (!$project) {
-           throw $this->createNotFoundException('Unable to find Project entity.');
+         $project = $this->model->find($id);
+         if (!$project) {
+             throw $this->createNotFoundException(
+                $this->translator->trans('No project found for id ') . $id
+             );
        }
 
-       $deleteForm = $this->createDeleteForm($id);
-
-       return array(
-           'project'      => $project,
-           'delete_form' => $deleteForm->createView(),
+       return $this->templating->renderResponse(
+           'AppBundle:Project:view.html.twig',
+           array('project' => $project)
        );
       }
 
     /**
      * @Route("/projects/new", name="project_new")
      */
-    public function newAction(Request $request)
+    public function createAction(Request $request)
     {
-        // 1) build the form
-        $project = new Project();
-        $form = $this->createCreateForm($project);
+        $projectForm = $this->formFactory->create(new ProjectType());
 
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $projectForm->handleRequest($request);
 
-            // 3) save the Project!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($project);
-            $em->flush();
-
-            // maybe set a "flash" success message for the user
-            $redirectUrl = $this->generateUrl('projects');
-
-            return $this->redirect($redirectUrl);
+        if ($projectForm->isValid()) {
+            $this->session->getFlashBag()->set(
+                'success',
+                $this->translator->trans('Saved')
+            );
+        } else {
+            $this->session->getFlashBag()->set(
+                'error',
+                $this->translator->trans('Not valid')
+            );
         }
 
-        return $this->render(
-            'AppBundle:Project:new.html.twig',
-            array('form' => $form->createView())
+        return $this->templating->renderResponse(
+         'AppBundle:Project:new.html.twig',
+         array('form' => $projectForm->createView())
         );
     }
 
     /**
-     * Creates a form to create a Project entity.
-     *
-     * @param Project $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Project $project)
-    {
-        $form = $this->createForm(new ProjectType(), $project, array(
-            'action' => $this->generateUrl('project_new'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
-    * Creates a form to edit a Project entity.
     *
-    * @param Project $entity The project
+    * @Route("/projects/edit/{id}", name="project_edit")
     *
-    * @return \Symfony\Component\Form\Form The form
     */
-    private function createEditForm(Project $project)
-    {
-        $form = $this->createForm(new ProjectType(), $project, array(
-            'action' => $this->generateUrl('project_update', array('id' => $project->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to edit an existing Project entity.
-     *
-     * @Route("/projects/{id}/edit", name="project_edit")
-     * @Method("GET")
-     * @Template()
-     */
     public function editAction($id)
     {
-        $em = $this->getDoctrine()->getManager();
-        $project = $em->getRepository('AppBundle:Project')->find($id);
-
-        if (!$project) {
-            throw $this->createNotFoundException('Unable to find Project entity.');
-        }
-
-        $editForm = $this->createEditForm($project);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'project'      => $project,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
     }
 
     /**
-     * Edits an existing Project entity.
-     *
-     * @Route("projects/{id}", name="project_update")
-     * @Method("PUT")
-     * @Template("AppBundle:Project:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
+    * @Route("/projects/delete/{id}", name="project_delete")
+    */
+    public function deleteAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
+      $id = $request->get('id', null);
+      $project = $this->model->find($id);
+      if (!$project) {
+           throw $this->createNotFoundException(
+               $this->translator->trans('No project found for id ') . $id
+           );
+       }
 
-        $project = $em->getRepository('AppBundle:Project')->find($id);
+       $projectForm = $this->formFactory->create(
+           new ProjectType(),
+           $project,
+           array(
+               'validation_groups' => 'project_delete'
+           )
+       );
 
-        if (!$project) {
-            throw $this->createNotFoundException('Unable to find Project entity.');
-        }
+       $projectForm->handleRequest($request);
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($project);
-        $editForm->handleRequest($request);
+       if ($projectForm->isValid()) {
+          remove($project);
+       }
 
-        if ($editForm->isValid()) {
-            $em->flush();
+       return $this->templating->renderResponse(
+           'AppBundle:Project:delete.html.twig',
+           array('form' => $currencyForm->createView())
+       );
 
-            return $this->redirect($this->generateUrl('project_edit', array('id' => $id)));
-        }
-
-        return array(
-            'project'      => $project,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-     * Deletes a Project entity.
-     *
-     * @Route("projects/{id}", name="project_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $project = $em->getRepository('AppBundle:Project')->find($id);
-
-            if (!$project) {
-                throw $this->createNotFoundException('Unable to find Project entity.');
-            }
-
-            $em->remove($project);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('projects'));
-    }
-
-    /**
-     * Creates a form to delete a Project entity by id.
-     *
-     * @param mixed $id The project id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('project_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
     }
 }
