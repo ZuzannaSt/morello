63cbbcd10b8dd9d2495ebd8aa00f319d8ca855a1
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-01-18 18:31:46 +0100

    Add custom query for further use

--- src/AppBundle/Repository/ProjectRepository.php
+++ src/AppBundle/Repository/ProjectRepository.php
@@ -28,9 +28,36 @@ class ProjectRepository extends EntityRepository
     public function findAllOrderedByName()
     {
         return $this->getEntityManager()
-            ->createQuery(
-                'SELECT p FROM AppBundle:Project p ORDER BY p.name ASC'
-            )
+            ->createQuery('
+                SELECT p
+                FROM AppBundle:Project p
+                ORDER BY p.name ASC
+            ')
             ->getResult();
+    }
+
+
+    public function findAllCustom()
+    {
+        $query = $this->createQueryBuilder('p')
+            ->select(
+                'p.id',
+                'p.name',
+                'u.username'
+            )
+            ->innerJoin(
+                'AppBundle:ProjectUser',
+                'pu',
+                'WITH',
+                'pu.project = p.id'
+            )
+            ->innerJoin(
+                'AppBundle:User',
+                'u',
+                'WITH',
+                'pu.user = u.id'
+            )
+            ->getQuery();
+        return $query->getResult();
     }
 }
