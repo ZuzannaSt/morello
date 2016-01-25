9acf73343166aa729939b80057d06c06fe93b57d
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-01-25 19:22:27 +0100

    Create Task Entity

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tasks")
+ */
+ class Task
+{
+    /**
+     * @ORM\Column(
+     *     type="integer",
+     *     nullable=false,
+     *     options={
+     *      "unsigned"=true
+     *     }
+     * )
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(
+     *     name="name",
+     *     type="string",
+     *     length=128,
+     *     nullable=false,
+     * )
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(
+     *     name="description",
+     *     type="string",
+     *     length=128,
+     *     nullable=true
+     * )
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
